f3ba696f377c37c0d4f1306386cc3b717924f330
Valcrist <github@valcrist.com>
2024-10-24 16:23:46 +0800

    fixed run_async_bg_tasks

--- toolbox/runner.py
+++ toolbox/runner.py
@@ -1,4 +1,5 @@
 import asyncio
+import threading
 from toolbox.utils import err, warn, exc
 
 
@@ -28,8 +29,19 @@ def run_async_tasks(*tasks):
 
 def run_async_bg_tasks(*coro_or_future):
     try:
-        loop = get_or_create_event_loop()
-        return [asyncio.ensure_future(task, loop=loop) for task in coro_or_future]
+
+        def run_in_thread(loop, tasks):
+            asyncio.set_event_loop(loop)
+            try:
+                return loop.run_until_complete(asyncio.gather(*tasks))
+            finally:
+                loop.close()
+
+        loop = asyncio.new_event_loop()
+        tasks = [asyncio.ensure_future(task, loop=loop) for task in coro_or_future]
+        thread = threading.Thread(target=run_in_thread, args=(loop, tasks))
+        thread.start()
+        return tasks
     except Exception as e:
         err(f"Failed to schedule background task(s): {e}")
         warn(exc())
