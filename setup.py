aa2a8092ff1284d50fd52dd7e6cdbc1b014c0381
Valcrist <github@valcrist.com>
2024-10-28 11:02:40 +0800

    added tzlocal to dependencies

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="toolbox",
-    version="0.1.19",
+    version="0.1.20",
     packages=find_packages(),
     install_requires=[
         "python-dotenv",
         "hexbytes",
         "colorlog",
         "pytz",
+        "tzlocal",
     ],
     url="https://github.com/Valcrist/toolbox",
     author="Valcrist",
