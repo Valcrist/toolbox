b10082cb57661e1cbc76e003475c5cde2a66f5cd
Valcrist <github@valcrist.com>
2026-04-17 14:59:16 +0800

    fixed shields

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="toolbox",
-    version="0.1.52",
+    version="0.1.53",
     packages=find_packages(),
     install_requires=[
         "python-dotenv",
